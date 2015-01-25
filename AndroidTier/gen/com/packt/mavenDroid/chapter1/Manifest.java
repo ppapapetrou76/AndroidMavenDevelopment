90ace48d051581af5aae827e072067fc3b41fc85
Patroklos Papapetrou <ppapapetrou76@gmail.com>
2015-01-25 15:26:05 +0200

    Clean up repo

--- AndroidTier/gen/com/packt/mavenDroid/chapter1/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.packt.mavenDroid.chapter1;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
--- service/src/main/java/com/packt/androidmaven/service/BookService.java
+++
@@ -1,8 +0,0 @@
-
-package com.packt.androidmaven.service;
-
-import com.packt.androidmaven.model.Book;
-
-public interface BookService {
-    Book createBook(String title, String format, String color, Integer numberOfPages);
-}
